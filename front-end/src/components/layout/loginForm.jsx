abb2ad643cd3d03c72cae20c3701695b9ad5b6e5
Juan Ardila <juardilad@unal.edu.co>
2025-09-27 19:05:35 -0500

    Fixing package.json

--- front-end/src/components/layout/loginForm.jsx
+++ front-end/src/components/layout/loginForm.jsx
@@ -4,14 +4,12 @@ import PasswordField from '../ui/passwordField';
 import SendFormButton from '../ui/sendFormButton';
 import styles from '../../styles/Form.module.css';
 
-export default function RegistrationForm() {
+export default function LoginForm() {
   return (
-    <form onSubmit={(e) => e.preventDefault()} className={styles.form} data-testid="registration-form">
+    <form onSubmit={(e) => e.preventDefault()} className={styles.formregistration} data-testid="login-form">
       <FormField label={"Correo"} testid={"email"} />
-      <FormField label={"Alias"} testid={"alias"} />
       <PasswordField label={"Contraseña"} testid={"password"} />
-      <PasswordField label={"Confirmar Contraseña"} testid={"confirm-password"} />
-      <SendFormButton label={"Registrarse"} testid={"send-button"} type={1} />
+      <SendFormButton label={"Iniciar Sesión"} testid={"send-button"} type={2} />
     </form>
   );
 }
